d26b74b1da846f82a45f27bac49d7cc0cc5db7a6
sivan-attu <aravinthattu2000@gmail.com>
2024-04-25 21:04:13 +0530

    dashboard-route-path-config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('../app/pages/dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
