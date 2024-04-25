7e2452dd821979622ae97aa8584ca615366e2991
sivan-attu <aravinthattu2000@gmail.com>
2024-04-25 20:58:29 +0530

    firebase-added-this-project

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'restaurant-management-sy-e5807',
+    appId: '1:892849175913:web:000c01ee47f7ba7acc51f8',
+    storageBucket: 'restaurant-management-sy-e5807.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyAAZ-lllU0IT_DcpQCdC3UoVSsOLmKTaGo',
+    authDomain: 'restaurant-management-sy-e5807.firebaseapp.com',
+    messagingSenderId: '892849175913',
+  },
+};
